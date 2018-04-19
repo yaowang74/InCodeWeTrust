1581a958f8afdebdec034964776f80107914f517
Wang Yao <yaowang74@gmail.com>
2018-04-19 00:45:33 -0400

    bug fix: if picture will be duplicated if next picture cannot be obtained

--- project_FGVC5/code/extract_url_from_json.py
+++ project_FGVC5/code/extract_url_from_json.py
@@ -1,8 +1,7 @@
 
 # coding: utf-8
 
-# In[ ]:
-
+# In[1]:
 
 """A script to handle kaggle FGVC5 json files
 
@@ -11,23 +10,24 @@
 """
 
 import requests
+import time
 import json
 import pandas as pd
 # from PIL import Image
 # from io import BytesIO
 
-JSON_FILE_PATH = "C:/Users/H211803/Documents/github/InCodeWeTrust/project_FGVC5/data/input/"
-IMAGE_PATH = "C:/Users/H211803/Documents/github/InCodeWeTrust/project_FGVC5/data/working/"
+JSON_FILE_PATH = "/home/wyao/github/InCodeWeTrust/project_FGVC5/data/input/"
+IMAGE_PATH = "/home/wyao/github/InCodeWeTrust/project_FGVC5/data/working/"
 TRAIN_FILE_NAME = "train.json"
 TEST_FILE_NAME = "test.json"
 VALI_FILE_NAME = "validation.json"
+MAX_RETRY = 3
 
 train_image_file = JSON_FILE_PATH + TRAIN_FILE_NAME
 # print(image_url_file)
 
 
-# In[ ]:
-
+# In[2]:
 
 def parse_json(json_file, json_key):
     """This function extracts image urls from json file.
@@ -57,15 +57,15 @@ def parse_json(json_file, json_key):
         return image_annotation_df
 
 
-# In[ ]:
-
+# In[3]:
 
 image_url_df = parse_json(train_image_file, 'images')
 image_annotations_df = parse_json(train_image_file, 'annotations')
+image_annotations_df.to_csv(JSON_FILE_PATH + "train_label.csv",
+                            index=False)
 
 
-# In[ ]:
-
+# In[4]:
 
 def show_image_by_id(image_dataframe, image_id, save = False):
     """This function show image by id.
@@ -101,30 +101,65 @@ def show_image_by_id(image_dataframe, image_id, save = False):
 show_image_by_id(image_url_df, 4, True)
 
 
-# In[ ]:
+# In[8]:
 
-
-def save_image_batch(image_dataframe):
+def save_image_batch(image_dataframe, dest, id_start_from=1):
     """This function show image in batch.
     
     :param image_dataframe: a data frame contains image id and image url.
     :type image_dataframe: pandas data frame
+    :param dest: dest possible values: ["train","test","validation"]
+    :type dest: str
+    :param id_start_from: image id starts from, has to be positive integer.
+    :type id_start_from: int
     :returns: 
     
     """
     num_image = image_dataframe.shape[0]
-    
-    for idx in range(num_image):
+    if dest == "train":
+        saving_path = IMAGE_PATH + "train/"
+    elif dest == "test":
+        saving_path = IMAGE_PATH + "test/"
+    elif dest == "validation":
+        saving_path = IMAGE_PATH + "validation/"
+    else:
+        raise ValueError("invalid destination arg.")
+
+    for idx in range(id_start_from-1, num_image):
         image_id = idx + 1
         url = image_dataframe.iloc[idx]['url']
 
         original_image_name = url.rsplit('/', 1)[1]
-        image_data = requests.get(url).content
-        new_image_name = str(image_id) + '.jpg'
-        new_image_path = IMAGE_PATH + new_image_name
+        retry = 0
+        image_retrieved = True
+        while retry < MAX_RETRY:
+            try:
+                image_data = requests.get(url,timeout=10).content
+                break
+            except:
+                retry += 1
+                print("Connection refused by the server..")
+                print("Let me sleep for 5 seconds")
+                print("ZZzzzz...")
+                time.sleep(5)
+                print("Was a nice sleep, now let me continue...")
+                if retry == MAX_RETRY:
+                    image_retrieved = False
+                    print("Failed to get {}.jpg".format(image_id))
+                continue
 
-        with open(new_image_path, 'wb') as handler:
-            handler.write(image_data)
-        print("Saved image: {} as {}.".format(original_image_name,
-                                              new_image_name))
+        if image_retrieved == True:
+            new_image_name = str(image_id) + '.jpg'
+            new_image_path = saving_path + new_image_name
+
+            with open(new_image_path, 'wb') as handler:
+                handler.write(image_data)
+            print("Saved image: {} as {}.".format(original_image_name,
+                                                  new_image_name))
+
+
+# In[7]:
+
+# save training image
+save_image_batch(image_url_df,"train",293)
 
